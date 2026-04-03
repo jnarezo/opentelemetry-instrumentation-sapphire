5d1eb9b8a2485581ee8dd88134a9c9515a68f6d1
jnarezo <jnarezo@ucsd.edu>
2026-04-03 12:42:45 +0000

    trim dependencies

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
-import type { InstrumentationConfig } from '@opentelemetry/instrumentation/build/src/types.js';
+import type { InstrumentationConfig } from '@opentelemetry/instrumentation';
 
 export interface SapphireInstrumentationConfig extends InstrumentationConfig {}
